75eac4c72e77be9075d6e469ffc2f97cb5a6c78b
danibanez <danibanez.info@gmail.com>
2023-07-03 17:03:36 +0200

    dataController refactoring

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -1,95 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const dataFiles = require('../config/dataFiles');
 const dataFetcher = require('../utils/dataFetcher.js');
 
-router.get('/:file', async (req, res) => {
-    const dataFiles = {
-        data_properties: 'data/data_properties.json',
-        object_properties: 'data/object_properties.json',
-        vars: 'data/vars.json',
-        nodes: 'data/nodes.json',
-    };
+const getVars = async (req, endpoint) => {
+    if (!req.session.varsPromise && !req.session.vars) {
+        req.session.varsPromise = dataFetcher.getVarsFromSPARQL(endpoint);
+        req.session.save();
+    }
 
-    const file = dataFiles[req.params.file];
-
-    if (file) {
-        let fileContent;
-        const endpoint = req.headers['x-sparql-endpoint'];
-
-        try {
-            switch (req.params.file) {
-                case 'vars':
-                    if (!req.session.varsPromise && !req.session.vars) {
-                        req.session.varsPromise = dataFetcher.getVarsFromSPARQL(endpoint);
-                        req.session.save();
-                    }
-
-                    if (!req.session.vars) {
-                        console.log("Vars fetched from server")
-                        fileContent = await req.session.varsPromise;
-                        req.session.vars = fileContent;
-                        //console.log(req.session.vars);
-                        req.session.save();
-                    } else {
-                        console.log("Vars fetched from session")
-                        fileContent = req.session.vars;
-                    }
-                    break;
-
-                case 'object_properties':
-                case 'data_properties':
-                    if (!req.session.varsPromise && !req.session.vars) {
-                        req.session.varsPromise = dataFetcher.getVarsFromSPARQL(endpoint);
-                        req.session.save();
-                    }
-
-                    let vars;
-                    if (!req.session.vars) {
-                        vars = await req.session.varsPromise;
-                        req.session.vars = vars;
-                        req.session.save();
-                    } else {
-                        vars = req.session.vars;
-                    }
-
-                    if (!req.session.propertiesPromise && (!req.session.object_properties || !req.session.data_properties)) {
-                        console.log("Properties fetched from server")
-                        req.session.propertiesPromise = dataFetcher.getPropertiesFromSPARQL(vars, endpoint);
-                        req.session.save();
-                    }
-
-                    let properties;
-                    if (!req.session.object_properties || !req.session.data_properties) {
-                        properties = await req.session.propertiesPromise;
-                        req.session.object_properties = properties.objectProperties;
-                        req.session.data_properties = properties.dataProperties;
-                        req.session.save();
-                    } else {
-                        console.log("Properties fetched from session")
-                        //console.log(req.session.object_properties);
-                        //console.log(req.session.data_properties);
-                    }
-
-                    fileContent = req.params.file === 'object_properties' ? req.session.object_properties : req.session.data_properties;
-                    break;
-
-                case 'nodes':
-                    const { filter } = req.query;
-                    // TODO
-                    fileHandler.sendPartialFile(res, file, filter);
-                    break;
-            }
-        } catch (err) {
-            return res.sendStatus(500);
-        }
-
-        if (req.params.file !== 'nodes') {
-            // Respond with file content from session
-            res.setHeader('Content-Type', 'application/json');
-            res.json(fileContent);
-        }
+    let vars;
+    if (!req.session.vars) {
+        vars = await req.session.varsPromise;
+        req.session.vars = vars;
+        req.session.save();
     } else {
-        res.sendStatus(404);
+        vars = req.session.vars;
+    }
+
+    return vars;
+};
+
+router.get('/:file', async (req, res, next) => {
+    const { file } = req.params;
+    const filePath = dataFiles[file];
+    const endpoint = req.headers['x-sparql-endpoint'];
+
+    if (!filePath) {
+        return res.sendStatus(404);
+    }
+
+    try {
+        let fileContent;
+
+        switch (file) {
+            case 'vars':
+                fileContent = await getVars(req, endpoint);
+                break;
+
+            case 'object_properties':
+            case 'data_properties':
+                const vars = await getVars(req, endpoint);
+
+                if (!req.session.propertiesPromise && (!req.session.object_properties || !req.session.data_properties)) {
+                    req.session.propertiesPromise = dataFetcher.getPropertiesFromSPARQL(vars, endpoint);
+                    req.session.save();
+                }
+
+                let properties;
+                if (!req.session.object_properties || !req.session.data_properties) {
+                    properties = await req.session.propertiesPromise;
+                    req.session.object_properties = properties.objectProperties;
+                    req.session.data_properties = properties.dataProperties;
+                    req.session.save();
+                }
+
+                fileContent = file === 'object_properties' ? req.session.object_properties : req.session.data_properties;
+                break;
+
+            case 'nodes':
+                const { filter } = req.query;
+                console.log("Filter nodes reached '" + filter + "'");
+                // TODO
+                break;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+        res.json(fileContent);
+    } catch (err) {
+        next(err);  // Pass the error to the error-handling middleware
     }
 });
 
