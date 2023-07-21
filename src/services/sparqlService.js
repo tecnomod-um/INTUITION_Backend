49bd1df22029e261feb97a779965db11fef13cef
danibanez <danibanez.info@gmail.com>
2023-07-21 11:10:29 +0200

    Query optimization halted. Server settings make it imposible to test the app.

--- src/services/sparqlService.js
+++ src/services/sparqlService.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const executeQuery = async (endpoint, query) => {
+    let startTime = Date.now();
     const options = {
         method: 'POST',
         url: endpoint,
@@ -16,8 +17,15 @@ const executeQuery = async (endpoint, query) => {
     try {
         const response = await axios.request(options);
         return response.data;
+        let endTime = Date.now();
+        let timeTaken = endTime - startTime;
+        console.log(`Time taken: ${timeTaken} milliseconds`);
     } catch (error) {
+        let endTime = Date.now();
+        let timeTaken = endTime - startTime;
+        console.log(`Time taken: ${timeTaken} milliseconds`);
         console.error("Error while executing query:", error.message);
+        console.error(query);
         if (error.response && error.response.data) {
             console.error("Response body:", error.response.data);
         }
